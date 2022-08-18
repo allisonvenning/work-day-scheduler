019f70743d416d8852c4aae73c35e2e06a71a963
Allison Venning <allisonvenning7@gmail.com>
2022-08-17 22:49:50 -0400

    created script js file

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //Moment.js code for current date and time
+    let NowMoment = moment().format("MMMM Do YYYY");
+    let displayDate = document.getElementById("currentDay");
+    displayDate.innerHTML = NowMoment;
+    let currentHour = moment().format("HH");
+});
