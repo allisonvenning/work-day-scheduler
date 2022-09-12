9ab4f2099e5f6980201f7a547679196ac652ded2
Allison Venning <allisonvenning7@gmail.com>
2022-09-12 14:00:42 -0400

    updated readme file

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    //Moment.js code for current date and time
-    let NowMoment = moment().format("MMMM Do YYYY");
-    let displayDate = document.getElementById("currentDay");
-    displayDate.innerHTML = NowMoment;
-    let currentHour = moment().format("HH");
-});
